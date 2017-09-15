1192b0961e51aefb0bba78ec91b3c25a7a2ae549
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-09-15 21:12:46 +0300

    Добавленны примеры работы System.arraycopy

--- src/question/question3/JavaUtilArrayUsage.java
+++ src/question/question3/JavaUtilArrayUsage.java
@@ -4,16 +4,23 @@ import java.util.Arrays;
 
 public class JavaUtilArrayUsage {
     public static void main(String[] args) {
+
+        /*
         int[] array = new int[] {1, 2, 1, 3, 3};
         int[] clone = Arrays.copyOf(array, array.length);
+        */
 
-        Arrays.sort(clone);
+        int[] array = new int[] {1, 2, 1, 3, 3};
+        int[] clone = new int[array.length];
 
+        System.arraycopy(array, 2, clone, 1, 3);
 
         // for
         for(int i = 0; i < clone.length; i++) {
             System.out.print("[" + i + "]" + clone[i] + " ");
         }
+
+        Arrays.sort(clone);
 
         System.out.println();
 
