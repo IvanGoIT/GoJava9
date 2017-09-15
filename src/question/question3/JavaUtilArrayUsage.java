7409fc838a39c2889df7cfc295260c690e28df5d
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-09-15 21:01:54 +0300

    JavaUtilArray example

---
+++ src/question/question3/JavaUtilArrayUsage.java
@@ -0,0 +1,25 @@
+package question.question3;
+
+import java.util.Arrays;
+
+public class JavaUtilArrayUsage {
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 2, 1, 3, 3};
+        int[] clone = Arrays.copyOf(array, array.length);
+
+        Arrays.sort(clone);
+
+
+        // for
+        for(int i = 0; i < clone.length; i++) {
+            System.out.print("[" + i + "]" + clone[i] + " ");
+        }
+
+        System.out.println();
+
+        // foreach
+        for(int x : clone) {
+            System.out.print(x + " ");
+        }
+    }
+}
