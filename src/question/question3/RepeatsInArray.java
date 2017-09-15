ad5192b2bb129b05eeaa4591ef8e6a2c0f74c255
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-09-15 20:38:19 +0300

    Поиск повторений числа в массиве

---
+++ src/question/question3/RepeatsInArray.java
@@ -0,0 +1,18 @@
+package question.question3;
+
+public class RepeatsInArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 2, 1, 3, 3};
+
+        int x = 3;
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            // проверить является ли array[i] равным X
+            // если является то count увеличить на 1
+            //
+        }
+
+        System.out.println("Кол-во повторений = " + count);
+    }
+}
