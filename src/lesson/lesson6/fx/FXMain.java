693780bb4b6fa591488911ae1b06be3ab16d308a
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-10-04 20:37:34 +0300

    Отрисовка простой линии в JavaFX

--- src/lesson/lesson6/fx/FXMain.java
+++ src/lesson/lesson6/fx/FXMain.java
@@ -1,6 +1,9 @@
 package lesson.lesson6.fx;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class FXMain extends Application{
@@ -11,6 +14,14 @@ public class FXMain extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         // our code
-        primaryStage.show();
+
+        Pane root = new Pane(); // создаем пустую панель
+        Line line = new Line(10, 10, 300, 300); // создаем линию
+        root.getChildren().addAll(line);  // кладем линию на пустую панель
+
+        Scene scene = new Scene(root); // создаем сцену и кладем внутрь root (пустую панельку)
+        primaryStage.setScene(scene); // привязываем окно программы к нашей сцене
+
+        primaryStage.show(); // отображаем окно
     }
 }
